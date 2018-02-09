ea6f9080225c2e0c0b2249bfaaef17aa46f3069b
Lukas Pustina <lukas@pustina.net>
2018-02-09 12:19:53 +0100

    Add regex matching for tag filters

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+extern crate regex;
+extern crate rusoto_core;
+extern crate rusoto_ec2;
+extern crate rusoto_sts;
+extern crate tabwriter;
+
+use regex::Regex;
+use rusoto_core::{default_tls_client, DefaultCredentialsProvider, Region};
+use rusoto_ec2::{Ec2, Ec2Client, DescribeInstancesRequest, Tag};
+use rusoto_sts::{StsClient, StsAssumeRoleSessionCredentialsProvider};
+use std::default::Default;
+use std::io::{self, Write};
+use tabwriter::TabWriter;
+
+
+pub fn run() {
+    let base_provider = DefaultCredentialsProvider::new().unwrap();
+    let sts = StsClient::new(default_tls_client().unwrap(), base_provider, Region::EuCentral1);
+
+    let provider = StsAssumeRoleSessionCredentialsProvider::new(
+        sts,
+        //"arn:aws:iam::959479900016:role/OrganizationAccountAccessRole".to_string(),
+        "arn:aws:iam::737288212407:role/OrganizationAccountAccessRole".to_string(),
+        "default".to_string(),
+        None, None, None, None
+    );
+
+    let client = Ec2Client::new(default_tls_client().unwrap(), provider, Region::EuCentral1);
+
+    let request = Default::default();
+    let result = client.describe_instances(&request).unwrap();
+
+    let mut tw = TabWriter::new(vec![]).padding(1);
+    writeln!(&mut tw, "  ID\t  Private IP\t  Public IP\t  Tags:Name").unwrap();
+    for resv in result.reservations.unwrap() {
+        //writeln!(&mut tw, "Reservation ID: '{}'", resv.reservation_id.as_ref().unwrap()).unwrap();
+        let instances = resv.instances.as_ref().unwrap().iter()
+            .filter(|i| has_tag(i.tags.as_ref().unwrap(), "Na..", Some("dns...")));
+        for i in instances {
+            writeln!(&mut tw, "| {}\t| {}\t| {}\t| {}\t|",
+                     i.instance_id.as_ref().unwrap(),
+                     i.private_ip_address.as_ref().unwrap(),
+                     i.public_ip_address.as_ref().unwrap(),
+                     get_name_from_tags(i.tags.as_ref().unwrap()).unwrap()
+                     ).unwrap();
+        }
+    }
+    let out_str = String::from_utf8(tw.into_inner().unwrap()).unwrap();
+
+    println!("{}", out_str);
+}
+
+fn get_name_from_tags(tags: &Vec<Tag>) -> Option<&String> {
+    let tag_name = Some("Name".to_string());
+    tags.iter()
+        .filter(|tag| tag.key == tag_name)
+        .take(1)
+        .last()
+        .map(|tag| tag.value.as_ref())
+        .and_then(|t| t)
+}
+
+/// Checks if tags contain a specific tag and optionally with a specific value
+/// Tag key and tag value are both matched using regular expressions
+///
+/// # Examples
+///
+/// ```
+/// # extern crate ceres;
+/// # extern crate rusoto_ec2;
+/// # use rusoto_ec2::Tag;
+/// # use ceres::has_tag;
+/// # fn main() {
+/// let tags = vec![ Tag{ key: Some("Name".to_string()), value: Some("Example Instance".to_string()) }];
+///
+/// let res = has_tag(&tags, "Name", None); // true
+/// # assert_eq!(res, true);
+///
+/// let res = has_tag(&tags, "Name", Some("Example Instance")); // true
+/// # assert_eq!(res, true);
+///
+/// let res = has_tag(&tags, "Name", Some("Example")); // true
+/// # assert_eq!(res, true);
+///
+/// let res = has_tag(&tags, "Name", Some("Example$")); // false
+/// # assert_eq!(res, false);
+///
+/// let res = has_tag(&tags, "NoSuchTag", None); // false
+/// # assert_eq!(res, false);
+/// # }
+/// ```
+pub fn has_tag(tags: &Vec<Tag>, key: &str, value: Option<&str>) -> bool {
+    let key_re = Regex::new(key).unwrap();
+    let pred: Box<Fn(&Tag) -> bool> = match (key, value) {
+        (_, None) => Box::new( |tag: &Tag| key_re.is_match(tag.key.as_ref().unwrap()) ),
+        (_, Some(v)) => {
+            let value_re = Regex::new(v).unwrap();
+            Box::new( move |tag: &Tag|
+                      key_re.is_match(tag.key.as_ref().unwrap())
+                      && value_re.is_match(tag.value.as_ref().unwrap())
+                    )
+        },
+    };
+    tags.iter().any(|tag| pred(tag))
+}
+
