2e2d147fce0c87d94f3c78c93e5ae98ff0cb82dd
Lukas Pustina <lukas@pustina.de>
2018-05-30 19:01:02 +0200

    Add module

---
+++ src/modules/ops/webserver/backup.rs
@@ -0,0 +1,32 @@
+use clap::{App, Arg, ArgMatches, SubCommand};
+use webbrowser;
+
+use config::CeresConfig as Config;
+use run_config::RunConfig;
+use modules::*;
+
+pub const NAME: &str = "browse";
+
+pub struct SubModule;
+
+impl Module for SubModule {
+    fn build_sub_cli() -> App<'static, 'static> {
+        SubCommand::with_name(NAME)
+            .about("Execute backup script remotely")
+    }
+
+    fn call(cli_args: Option<&ArgMatches>, run_config: &RunConfig, config: &Config) -> Result<()> {
+        let args = cli_args.unwrap(); // Safe unwrap
+        do_call(args, run_config, config)
+    }
+}
+
+fn do_call(args: &ArgMatches, run_config: &RunConfig, config: &Config) -> Result<()> {
+    let profile = match run_config.active_profile.as_ref() {
+        "default" => config.get_default_profile(),
+        s => config.get_profile(s),
+    }.chain_err(|| ErrorKind::ModuleFailed(NAME.to_owned()))?;
+
+
+    Ok(())
+}
